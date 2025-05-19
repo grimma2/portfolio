58ab18ce0bd4a1a3d0ec22ca8729d2781bd9288f
Andrey Chella <achella@servicepipe.ru>
2025-05-19 23:29:27 +0300

    Add Yandex Metrika tracking script: integrate analytics for improved user insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto, Montserrat } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
 const roboto = Roboto({
   subsets: ["latin", "cyrillic"],
@@ -27,6 +28,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" suppressHydrationWarning>
+      <head>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+            ym(102000922, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/102000922" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+      </head>
       <body className={`${roboto.variable} ${montserrat.variable} font-sans`}>
         <ThemeProvider
           attribute="class"
